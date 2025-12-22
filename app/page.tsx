76581c56dc3e3e80cd48962fc5a013aa5f5ef12d
Ahmet Kerem Tanrıverdi <ahmetk.tanriverdi@gmail.com>
2025-12-22 19:43:10 +0300

    landing page and responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,171 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ChartPie, Dot, FileChartLine, FileCheck, Layers, Rocket, Shield, TrendingUpIcon, Wallet } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="sm:w-1/2 p-3 mx-auto mt-10 space-y-10">
+      <div className="flex items-center gap-10 justify-between">
+        <div className="text-center sm:text-start">
+          <Badge variant='default' className="bg-violet-500"><Dot className="mr-2" size={20} /> New Generation Finance</Badge>
+          <h2 className="lg:text-6xl font-bold">Discover Freedom in <br />
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-200 to-violet-600">Finance</span>
+          </h2>
+          <p className="text-muted-foreground lg:text-lg">The most modern way to track your expenses, manage your budget wisely, and secure your future. </p>
+          <br />
+          <Link className="bg-violet-700 p-3 rounded-md text-black hover:bg-violet-800" href="/dashboard">
+            Get Started
+          </Link>
+        </div>
+        <div className="h-1/2 w-1/2 hidden md:block lg:block">
+          <Image src={'/logotrack.png'} alt="logo" width={800} height={800}
+            className="border-2 border-green-800 rounded-2xl mt-2"
+          />
+        </div>
+        <div>
+        </div>
+      </div>
 
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      <div className="mt-30">
+        <h2 className="text-2xl text-center font-bold mb-10">Why Choose New Generation Finance?</h2>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-3">
+          <li >
+            <Card
+              className="h-full w-full bg-purple-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100">
+              <CardHeader>
+                <CardTitle className="flex items-center gap-2">
+                  <FileCheck className="mr-2" color="green" size={30} /> Intuitive Expense Tracking
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                Easily log and categorize your expenses with our user-friendly interface.
+              </CardContent>
+            </Card>
+          </li>
+
+          <li >
+            <Card className="h-full w-full bg-purple-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100">
+              <CardHeader>
+                <CardTitle className="flex items-center gap-2">
+                  <FileCheck className="mr-2" color="green" size={30} /> Comprehensive Budget Management
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                Set budgets, monitor spending, and receive insights to stay on track.
+              </CardContent>
+            </Card>
+          </li>
+
+          <li >
+            <Card className="h-full w-full bg-purple-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100">
+              <CardHeader>
+                <CardTitle className="flex items-center gap-2">
+                  <Shield className="mr-2" color="green" size={30} /> Secure and Private
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                Your financial data is protected with top-notch security measures.
+              </CardContent>
+            </Card>
+          </li>
+
+          <li>
+            <Card className="h-full w-full bg-purple-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100">
+              <CardHeader>
+                <CardTitle className="flex items-center gap-2">
+                  <TrendingUpIcon className="mr-2" color="green" size={30} />
+                  <h2>Future Planning Tools</h2>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                Future Planning Tools: Utilize our tools to plan for savings, investments, and financial goals.
+              </CardContent>
+            </Card>
+          </li>
+        </ul>
+      </div>
+
+      <section id="features">
+        <div className="mt-30 text-center">
+          <h4 className="text-violet-600 font-bold mb-2">Features
+            <br />
+            <span className="text-white">The smartest way to manage your finances</span>
+          </h4>
+          <p className="text-muted-foreground">All the tools you need to achieve your financial goals, in one place,
+            <br />
+            without complicated tables.</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid px-5 transition-colors hover:border-transparent dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-          <a href="dashboard" className="text-white">dash</a>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mx-auto mt-10">
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <FileChartLine className="mb-4 h-8 w-8 text-violet-600" /> <br />
+                Easy Expense Tracking
+              </CardTitle>
+              <CardContent>
+                <p>Record your expenses in seconds. Instantly see how much you spent on what and get an analysis report with AI-powered categorization.</p>
+              </CardContent>
+            </CardHeader>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <Wallet className="mb-4 h-8 w-8 text-violet-600" /> <br />
+                Smartest Budget Management
+              </CardTitle>
+              <CardContent>
+                <p>Set monthly limits and reach your goals. Eliminate surprise spending with overdraft alerts.</p>
+              </CardContent>
+            </CardHeader>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <ChartPie className="mb-4 h-8 w-8 text-violet-600" /> <br />
+                Deeply Insightful Reports
+              </CardTitle>
+              <CardContent>
+                <p>Discover where your money is going with visual charts and reports. Identify savings opportunities.</p>
+              </CardContent>
+            </CardHeader>
+          </Card>
         </div>
-      </main>
+      </section>
+
+      <div>
+        <Card className="text-center mt-10">
+          <CardHeader className="text-nowrap">
+            <CardTitle className="lg:text-6xl">
+              <Rocket className="mx-auto mb-6" color="violet" size={40} />
+              <h2>
+                Start managing your <br />
+                <span className="text-violet-600">
+                  Money Today.
+                </span>
+              </h2>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="mt-4 mb-8 text-muted-foreground">
+              Join New Generation Finance and take control of your financial future. Sign up now and exerience the difference!
+            </p>
+            <CardAction className="w-1/2 mx-auto">
+              <Link href={'/dashboard'}>
+                <Button className="bg-violet-700 rounded-md w-full" size={'lg'}>
+                  Get Started Now
+                </Button>
+              </Link>
+            </CardAction>
+          </CardContent>
+        </Card>
+      </div>
+
+
     </div>
   );
 }
