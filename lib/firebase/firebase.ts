3e617426d48871f0f182a7e22f645deb0a08eb23
Kerem Tanriverdi <ahmetk.tanriverdi@gmail.com>
2025-12-28 02:58:03 +0300

    changes firebase.json to .env

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 import jsonFirebase from '../../firebase.json'
 
 const clientCredentials = {
-    apiKey: jsonFirebase.apiKey,
-    authDomain: jsonFirebase.authDomain,
-    projectId: jsonFirebase.projectId,
-    storageBucket: jsonFirebase.storageBucket,
-    messagingSenderId: jsonFirebase.messagingSenderId,
-    appId: jsonFirebase.appId,
-    measurementId: jsonFirebase.measurementId
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(clientCredentials) : getApp();
