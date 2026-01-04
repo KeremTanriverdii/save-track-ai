0ee86416d5e5b96ec1fd167f33b60fe3b9d9b722
Kerem Tanriverdi <ahmetk.tanriverdi@gmail.com>
2026-01-04 18:22:16 +0300

    config changes to firebase-admin (2)

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const nextConfig: NextConfig = {
       }
     ]
   },
+  turbopack: {},
   serverExternalPackages: ['firebase-admin', '@google-cloud/storage'],
   experimental: {
     serverActions: {
